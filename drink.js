0da44562c5573d209be8b735b8d7113b0b191f20
Tushar Singh <56396090+mr-sing@users.noreply.github.com>
2020-06-02 15:59:00 +0530

    new

--- drink.js
+++ drink.js
@@ -1,11 +1,11 @@
 var secretnumber = 4;
 var stringGuess = prompt("guess a number");
 var guess = Number(stringGuess);
-if(Number(guess) === secretnumber)
+if(guess === secretnumber)
 {
 	alert("you got it!");
 }
-else if (Number(guess) > secretnumber)
+else if (guess > secretnumber)
 {
 	alert("too high! ");
 }
